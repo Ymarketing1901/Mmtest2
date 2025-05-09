c0e7b24a84579d91c0b5470367d8e696d918570a
Ymarketing1901 <ymarketing@gmail.com>
2025-05-09 13:08:07 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById("app");
-  app.innerHTML = "<h1>Hello, the game is working!</h1><button id='testBtn'>Click me</button>";
-  document.getElementById("testBtn").onclick = () => {
-    app.innerHTML += "<p>Button clicked. Everything's running fine.</p>";
-  };
-  console.log("JS Loaded Successfully");
-});
