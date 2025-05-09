7d30d43c1f8c9037c34be7f38df7d71b0d7349dc
Ymarketing1901 <ymarketing@gmail.com>
2025-05-09 13:07:54 -0400

    Delete grade_1_hospital.js

--- grade_1_hospital.js
+++
@@ -1,6 +0,0 @@
-const grade_1_hospital = {
-  easy: [
-    { text: "What is 2 + 2?", options: ["3", "4", "5", "6"], answer: "4" },
-    { text: "What is 5 - 3?", options: ["2", "3", "4", "1"], answer: "2" }
-  ]
-};
